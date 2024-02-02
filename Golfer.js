36150f28c58726d873cebfededc9d61d3cafce55
Hunter Monroe <huntis@outlook.com>
2024-02-01 19:40:01 -0700

    Iteration 0.1

---
+++ Golfer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react'; 
+
+
+// const [golfer, setGolfer] = useState({});
+
+// const createGolfer = (name) => {
+//   setGolfer({
+//     id: 1,
+//     name: name,
+//     golfBag: {}, 
+//   });
+// };
+
+// export default golfer;
+// export default createGolfer; 
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Text, View, Button, StyleSheet } from 'react-native';
+// import golfer from './Golfer'
 
 const HomeScreen = ({ navigation }) => {
+  const [golfer, setGolfer] = useState({});
+
+  const createGolfer = (name) => {
+     setGolfer({
+       id: 1,
+       name: name,
+       golfBag: {}, 
+  });
+};
+
+  
+
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+      
+    
     },
     text: {
       borderColor: 'red',
       borderWidth: 2,
     },
-    
+
+
   });
   return (
     <View style={styles.container}>
