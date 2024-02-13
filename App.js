24f8fe9d9bd2724556ee029a5aa47a35597a717d
Hunter Monroe <huntis@outlook.com>
2024-02-12 21:30:47 -0700

    Started-golfbag

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { StrictMode, useState } from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,42 +7,28 @@ import HomeScreen from './HomeScreen';
 import Profile from './Profile';
 import golfCart from './images/golf-cart.png';
 import ModalDemo from './Modal';
+import ReactDOM from 'react-dom/client';
+//  const root = ReactDOM.createRoot(document.getElementById('root'));
+//  root.render(<App />);
 
 export default function App() {
+ 
   const [isHappy, setIsHappy] = useState(true);
   const Stack = createNativeStackNavigator();
   const cart = golfCart;
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen
-          name="HomeScreen"
-          component={HomeScreen}
-          data={isHappy}
-          options={{ title: 'Home' }}
-        /> */}
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{ title: 'Profile' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            data={isHappy}
+            options={{ title: 'Home' }}
+          /> */}
+          <Stack.Screen name="Profile" component={Profile} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
   );
 }
 
-//  <View style={styles.container}>
-//       <Text style={styles.text}>Here we go!</Text>
-//       <Button title="hello" onPress={() => {
-//         setIsHappy(false
-//         )
-//       }}> </Button>
-//       <Text> We are doing aweso {isHappy ? 'yessss' : 'Nooo'} </Text>
-//       <StatusBar style="auto" />
-//       <Image
-//         source={{
-//           uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
-//         }}
-//         style={{ width: 200, height: 200 }}
-//       />
-//     </View>
--- Profile.js
+++ Profile.js
@@ -10,12 +10,15 @@ import {
   StyleSheet,
   Image,
   TextInput,
+  Pressable,
 } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 
 
 const Profile = ({ navigation, route }) => {
-  // const [number, setNumber] = useState('');
+  const [club, setClub] = useState();
+  const [yards, setYards] = useState()
 
   const styles = StyleSheet.create({
     container: {
@@ -24,10 +27,9 @@ const Profile = ({ navigation, route }) => {
       flex: 1,
       backgroundColor: 'black',
       color: 'white',
-      justifyContent: "center",
+      justifyContent: 'center',
       flexDirection: 'column',
-      border:'10px, solid, White ', 
-      
+      border: '10px, solid, White ',
     },
     text: {
       color: 'white',
@@ -38,54 +40,94 @@ const Profile = ({ navigation, route }) => {
     },
     clubBox: {
       height: '35rem',
-      width:'20rem', 
-      display:'flex',
+      width: '20rem',
+      display: 'flex',
       justifyContent: 'space-evenly ',
-      alignItems:'center',
+      alignItems: 'center',
       border: '10px solid white',
-      alignContent:"space-around"
-    }
+      alignContent: 'space-around',
+    },
+    head: {
+      color: 'green',
+      fontSize: 40,
+      fontWeight: 'bold',
+      border: 'solid ', 
+      borderBottom: '2px solid green',
+      borderTop: 'none'
+    },
   });
 
   const [golfer, setGolfer] = useState({
     id: 1,
     name: 'Hunter',
       golfBag: [
-        {driver: 200},
-        { '3wood': `1300 yards` },
-        
+        { driver: 200 },
+       {'3wood': `1300 yards` },
       ],
   });
 
+  const golfClubs = [
+    'Driver', '3-wood', '5-wood', '7-wood', '3-hybrid',
+    '4-hybrid', '1-iron', '2-iron', '3-iron', '4iron',
+    '5-iron', '6-iron', '7-iron', '8-iron', '9-iron',
+    'Pitching Wedge', 'Gap Wedge', 'Sand Wedge', 'Lob wedge', 'Putter']
+    console.log(golfClubs)
+
   const displayGolfer = () => {
-    
+  }
+  const createGolfBag = () => { 
+
   }
 
-  const createGolfer = (name) => {
-    setGolfer({
-      id: 1,
-      name: name,
-      golfBag: [
-        {
-          driver: 100,
-          '3wood': `1300 yards`
-        },
-      ],
-    });
-  }
+  // const createGolfer = (name) => {
+  //   setGolfer({
+  //     id: 1,
+  //     name: name,
+  //     golfBag: [
+  //       {
+  //         driver: 100,
+  //       }
+  //        { '3wood': `1300 yards` }
+  //       },
+  //     ],
+  //   });
+  // }
+ const addGolfClub = (clubss, distance) => {
+   golfer.golfBag.push({ 'clubss': distance });
+ };
+
   const bagCreate = golfer.golfBag.map((bag) => bag); 
   const createBag = bagCreate.map(bag => bag)
   console.log(golfer.golfBag.map((bag) => bag))
   console.log(bagCreate[0])
   console.log(createBag[0])
+  console.log(golfer)
       
   return (
     <View style={styles.container}>
       <View style={styles.clubBox}>
-        <Text style={styles.text}> Clubs </Text>
-        <Text style={styles.text} data={bagCreate['driver']}>
-         this is bag  {createBag[1]['3wood']}
-        </Text>
+        <Text style={styles.head}> Clubs </Text>
+          <RNPickerSelect
+              onValueChange={(value) => setClub(value)}
+              items={[
+                { label: 'Driver', value: 'Driver' },
+                { label: '3-Wood', value: '3-Wood' },
+                { label: '4-wood', value: '4-wood' },
+              ]}
+            />
+        <RNPickerSelect
+              onValueChange={(value) => setYards(value)}
+              items={[
+                { label: 1, value: 1 },
+                { label: 2, value: 2 },
+                { label: 3, value: 3 },
+              ]}
+            />
+        <Pressable style={styles.head} onPress={addGolfClub(club, yards)}>
+          <Text style={styles.head}>Add a club</Text>
+        </Pressable>
+
+        {/* <Text style={styles.text} > {golfClubs.map((club) => club.split(' ')) }</Text> */}
         <Text style={styles.text}>Driver {golfer.golfBag.driver} yards </Text>
         <Text style={styles.text}>Driver {golfer.golfBag.driver} yards </Text>
         <Text style={styles.text}>Driver {golfer.golfBag.driver} yards </Text>
